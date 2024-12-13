370411d5580b2d894b4bcb3ca4a98bf1c935c340
Damian <jerezdamian640@gmail.com>
2024-12-13 11:40:21 -0500

    refactored search with parameters to one method with streams

--- src/main/java/com/pluralsight/dealership_spring/VehicleController.java
+++ src/main/java/com/pluralsight/dealership_spring/VehicleController.java
@@ -34,56 +34,36 @@ public class VehicleController {
     }
 
     // Delete
-    @RequestMapping(method = RequestMethod.DELETE,path = "/deleteid/{id}")
+    @RequestMapping(method = RequestMethod.DELETE,path = "/delete/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable(name = "id") Integer vin){
         vDao.removeVehicle(vin);
     }
 
     // Search for a vehicle with parameters
-    // TODO results come up blank
-    @GetMapping(path = "/search/{vin}")
-    public List<Vehicle> searchByVin(@PathVariable(name = "vin") Integer vin){
-        return vDao.findVehicleByVin(vin);
+    @GetMapping("/search")
+    public List<Vehicle> searchByAll(@RequestParam(required = false) String make,
+                                     @RequestParam(required = false) String model,
+                                     @RequestParam(required = false) String type,
+                                     @RequestParam(required = false) String color,
+                                     @RequestParam(required = false) Integer year,
+                                     @RequestParam(required = false) Integer vin,
+                                     @RequestParam(required = false) Double minPrice,
+                                     @RequestParam(required = false) Double maxPrice,
+                                     @RequestParam(required = false, defaultValue = "0") Integer minMileage,
+                                     @RequestParam(required = false, defaultValue = "99999999") Integer maxMileage){
+        List<Vehicle> vehicleList = vDao.findAllVehicle();
+        return vehicleList.stream()
+                .filter(vehicle -> vin == null || vin == vehicle.getVin())
+                .filter(vehicle -> make == null || make.equalsIgnoreCase(vehicle.getMake()))
+                .filter(vehicle -> model == null || model.equalsIgnoreCase(vehicle.getModel()))
+                .filter(vehicle -> type == null || type.equalsIgnoreCase(vehicle.getVehicleType()))
+                .filter(vehicle -> color == null || color.equalsIgnoreCase(vehicle.getColor()))
+                .filter(vehicle -> year == null || year == vehicle.getYear())
+                .filter(vehicle -> minPrice == null || minPrice <= vehicle.getPrice())
+                .filter(vehicle -> maxPrice == null || maxPrice >= vehicle.getPrice())
+                .filter(vehicle -> minMileage == null || minMileage <= vehicle.getOdometer())
+                .filter(vehicle -> maxMileage == null || maxMileage >= vehicle.getOdometer()).toList();
     }
-
-    @GetMapping(path = "/search/make/{make}")
-    public List<Vehicle> searchByMake(@PathVariable(name = "make") String make){
-
-
-        return vDao.findVehicleByMake(make);
-    }
-
-    @GetMapping(path = "/search/model/{model}")
-    public List<Vehicle> searchByModel(@PathVariable(name = "model") String model){
-        return vDao.findVehicleByModel(model);
-    }
-
-    @GetMapping(path = "/search/type/{type}")
-    public List<Vehicle> searchByType(@PathVariable(name = "type") String type){
-        return vDao.findVehiclesByType(type);
-    }
-
-    @GetMapping(path = "/search/color/{color}")
-    public List<Vehicle> searchByColor(@PathVariable(name = "color") String color){
-        return vDao.findVehicleByColor(color);
-    }
-
-    @GetMapping(path = "/search/year/{year}")
-    public List<Vehicle> searchByYear(@PathVariable(name = "year") Integer year){
-        return vDao.findVehicleByYear(year);
-    }
-
-    @GetMapping(path = "/search/price")
-    public List<Vehicle> searchByPrice(@RequestParam(value = "minPrice") Double minPrice, @RequestParam(value = "maxPrice") Double maxPrice){
-        return vDao.findVehiclesByPriceRange(minPrice, maxPrice);
-    }
-
-    @GetMapping(path = "/search/mileage")
-    public List<Vehicle> searchByMileage(@RequestParam(value = "minMileage") Integer minMileage, @RequestParam(value = "maxMileage") Integer maxMileage){
-        return vDao.findVehiclesByMileage(minMileage, maxMileage);
-    }
-
-
 
 }
